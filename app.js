904dcf48432bbe17af584529439b3e9fceddcb23
Faryyaz <dilfary@hotmail.com>
2019-06-30 12:59:10 +0400

    Types learning

--- app.js
+++ app.js
@@ -1,2 +1,49 @@
 "use strict";
-alert("It's working!");
+// Types
+// string
+var fName = "Tommy";
+//number
+var mAge = 60;
+//boolean
+var married = true;
+//array
+var food = ["peach", "lemon"];
+var address = [7, "street"];
+//tuples
+var stuff = ["things", 7];
+//enum
+var color;
+(function (color) {
+    color[color["green"] = 0] = "green";
+    color[color["blue"] = 1] = "blue";
+    color[color["red"] = 100] = "red";
+    color[color["yellow"] = 101] = "yellow"; // Automatically becomes 101, it increments the last number
+})(color || (color = {}));
+var myColor = color.blue;
+console.log(myColor); // returns 1
+var myColor2 = color.yellow;
+console.log(myColor2); // returns 101
+var test;
+(function (test) {
+    test["hey"] = "hello";
+    test["phrase"] = "world";
+})(test || (test = {}));
+console.log(test.hey); // hello
+console.log(test.phrase); // world
+// any
+var car = "BMW";
+car = { brand: "BMW", color: "White" };
+//function
+function printName() {
+    return fName;
+}
+console.log(printName); // Tommy
+//void
+function sayHello() {
+    console.log("Hello"); // Hello
+}
+//params types
+function multiply(val1, val2) {
+    return val1 * val2;
+}
+console.log(multiply(2, 7));
--- app.ts
+++ app.ts
@@ -1,1 +1,64 @@
-alert("It's workixng!");
+// Types
+
+// string
+let fName: string = "Tommy";
+
+//number
+let mAge: number = 60;
+
+//boolean
+let married: boolean = true;
+
+//array
+let food: string[] = ["peach", "lemon"];
+let address: any[] = [7, "street"];
+
+//tuples
+let stuff:[string, number] = ["things", 7];
+
+//enum
+enum color {
+    green, // 0
+    blue, // 1
+    red = 100, // 100,
+    yellow // Automatically becomes 101, it increments the last number
+}
+
+let myColor: color = color.blue;
+console.log(myColor); // returns 1
+
+let myColor2: color = color.yellow;
+console.log(myColor2); // returns 101
+
+enum test {
+    hey = "hello",
+    phrase = "world"
+}
+
+console.log(test.hey); // hello
+console.log(test.phrase); // world
+
+
+// any
+let car: any = "BMW";
+car = {brand: "BMW", color: "White"};
+
+
+//function
+function printName(): string {
+    return fName;
+}
+
+console.log(printName); // Tommy
+
+//void
+function sayHello(): void {
+    console.log("Hello"); // Hello
+}
+
+//params types
+function multiply(val1: number, val2: number): number {
+    return val1 * val2;
+}
+
+console.log(multiply(2, 7)); // returns 14
