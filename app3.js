5384343adfeec43ad87ff9eb865eaa53f1e1ab47
Faryyaz <faryyaz@extension-interactive.com>
2019-07-11 15:50:58 +0400

    Compile app

--- app3.js
+++ app3.js
@@ -1,4 +1,20 @@
-"use strict";
+var MyMath;
+(function (MyMath) {
+    var PI = 3.142;
+    function calcCircumference(diameter) {
+        return PI * diameter;
+    }
+    MyMath.calcCircumference = calcCircumference;
+})(MyMath || (MyMath = {}));
+var MyMath;
+(function (MyMath) {
+    function calcArea(width, length) {
+        if (width === void 0) { width = 10; }
+        if (length === void 0) { length = 20; }
+        return width * length;
+    }
+    MyMath.calcArea = calcArea;
+})(MyMath || (MyMath = {}));
 /// <reference path="circleMath.ts" />
 /// <reference path="rectangularMath.ts" />
 console.log(MyMath.calcArea());
