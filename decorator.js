37ea4fa14eb50a5a95f9cc313a7fd0eb9a13ca25
Faryyaz <dilfary@hotmail.com>
2019-08-05 15:36:37 +0400

    Added more decorator functions

--- decorator.js
+++ decorator.js
@@ -5,6 +5,9 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
+var __param = (this && this.__param) || function (paramIndex, decorator) {
+    return function (target, key) { decorator(target, key, paramIndex); }
+};
 var logged = function (constructorFn) {
     console.log(constructorFn);
 };
@@ -25,7 +28,79 @@ var Portal = /** @class */ (function () {
     function Portal() {
     }
     Portal = __decorate([
-        logging(false)
+        logging(false) // cast logging
     ], Portal);
     return Portal;
 }());
+//advance
+function printable(constructorFn) {
+    constructorFn.prototype.print = function () {
+        console.log(this);
+    };
+}
+var Planting = /** @class */ (function () {
+    function Planting() {
+        this.name = "Green herb";
+    }
+    Planting = __decorate([
+        printable
+    ], Planting);
+    return Planting;
+}());
+var planting = new Planting();
+planting.print();
+//Method decorator
+function editable(value) {
+    return function (target, propName, descriptor) {
+        descriptor.writable = value;
+    };
+}
+//property decorator
+function overwritable(value) {
+    return function (target, propName) {
+        var newDescriptor = {
+            writable: value
+        };
+        return newDescriptor;
+    };
+}
+// parameter decorator
+function printInfo(target, methodName, parameterIndex) {
+    console.log("target: ", target);
+    console.log("methodName: ", methodName);
+    console.log("parameterIndex: ", parameterIndex);
+}
+var Renovation = /** @class */ (function () {
+    function Renovation(name) {
+        this.name = name;
+    }
+    Renovation.prototype.calcBudget = function () {
+        console.log("20M");
+    };
+    Renovation.prototype.printBudget = function (mode, printAll) {
+        if (printAll) {
+            console.log(10000);
+        }
+        else {
+            console.log(4777);
+        }
+    };
+    __decorate([
+        overwritable(true)
+    ], Renovation.prototype, "name", void 0);
+    __decorate([
+        editable(true)
+    ], Renovation.prototype, "calcBudget", null);
+    __decorate([
+        __param(1, printInfo)
+    ], Renovation.prototype, "printBudget", null);
+    return Renovation;
+}());
+var renovation = new Renovation("Super renovation");
+renovation.calcBudget();
+renovation.calcBudget = function () {
+    console.log("80M");
+};
+renovation.calcBudget();
+console.log(renovation);
+renovation.printBudget("any", true);
--- decorator.ts
+++ decorator.ts
@@ -18,7 +18,79 @@ let logging = (value: boolean): _logged | null => {
     return value ? logged : null;
 }
 
-@logging(false)
+@(<any>logging(false)) // cast logging
 class Portal {
 
 }
+
+//advance
+function printable(constructorFn: Function) {
+    constructorFn.prototype.print = function() {
+        console.log(this);
+    }
+}
+
+@printable
+class Planting {
+    name: string = "Green herb";
+}
+
+const planting = new Planting();
+(<any>planting).print();
+
+//Method decorator
+function editable(value: boolean) {
+    return function(target: any, propName: string, descriptor: PropertyDescriptor) {
+        descriptor.writable = value;
+    }
+}
+
+//property decorator
+function overwritable(value: boolean): any {
+    return function(target: any, propName: string) {
+        const newDescriptor: PropertyDescriptor = {
+            writable: value
+        }
+        return newDescriptor;
+    }
+}
+
+// parameter decorator
+function printInfo(target: any, methodName: string, parameterIndex: number) {
+    console.log("target: ", target);
+    console.log("methodName: ", methodName);
+    console.log("parameterIndex: ", parameterIndex);
+}
+
+class Renovation {
+    @overwritable(true)
+    name: string;
+
+    constructor(name: string) {
+        this.name = name;
+    }
+
+    @editable(true)
+    calcBudget() {
+        console.log("20M");
+    }
+
+    printBudget(mode: string, @printInfo printAll: boolean) {
+        if(printAll) {
+            console.log(10000);
+        } else {
+            console.log(4777);
+        }
+    }
+
+
+}
+
+const renovation = new Renovation("Super renovation");
+renovation.calcBudget();
+renovation.calcBudget = function() { // if editable true, we can overwrite the function.
+    console.log("80M");
+}
+renovation.calcBudget();
+console.log(renovation);
+renovation.printBudget("any", true);
